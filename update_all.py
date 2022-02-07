44589c4cafbf49ba928c03c64bcd741add2f1e99
adam.mandelson@gmail.com <adam.mandelson@gmail.com>
2022-02-07 16:50:44 -0500

    Cosmetic changes for update_all script

--- update_all.py
+++ update_all.py
@@ -8,115 +8,99 @@ on the relevant Google Sheet.
 '''
 
 
-from pathlib import Path
-import os
-
 from pday.data import Data
 from pday.query import PlandayQuery
 from pday.sheets_client import SheetsClient
-
-
-def auth_dir() -> Path:
-    '''
-    Returns a Path object with the location of the config folder.
-    '''
-    return Path('config/')
-
-
-def data_dir() -> Path:
-    '''
-    Returns a Path object with the location of the data folder.
-    Makes a new one if it doesn't exist.
-    '''
-    if not os.path.exists('./data/'):
-        os.makedirs('./data/')
-    return Path('data/')
+from pday.utils import auth_dir, data_dir
 
 
 # Instantiate PlandayQuery and Google Sheets objects
 planday_obj = PlandayQuery(auth_dir=auth_dir())
 google_client = SheetsClient(auth_dir=auth_dir())
+data_obj = Data()
 
-# Employees_list
-Data.save_to_json(
-    planday_query=planday_obj.get_employees,
+# Get absence requests
+absence_query = planday_obj.get_absence_requests
+data_obj.save_to_json(
+    planday_query=absence_query,
     data_dir=data_dir(),
-    title='employees_list.json'
+    title='absence_requests.json'
 )
-Data.save_to_sheets(
-    planday_query=planday_obj.get_employees,
+data_obj.save_to_sheets(
+    planday_query=absence_query,
     google_client=google_client,
-    sheet_name='employees_list'
-)
-
-# Departments_list
-Data.save_to_json(
-    planday_query=planday_obj.get_departments,
-    data_dir=data_dir(),
-    title='departments_list.json'
-)
-Data.save_to_sheets(
-    planday_query=planday_obj.get_departments,
-    google_client=google_client,
-    sheet_name='departments_list'
-)
-
-# Employee groups
-Data.save_to_json(
-    planday_query=planday_obj.get_employee_groups,
-    data_dir=data_dir(),
-    title='employee_groups.json'
-)
-Data.save_to_sheets(
-    planday_query=planday_obj.get_employee_groups,
-    google_client=google_client,
-    sheet_name='employee_groups'
-)
-
-# Get shift types
-Data.save_to_json(
-    planday_query=planday_obj.get_shift_types,
-    data_dir=data_dir(),
-    title='shift_types.json'
-)
-Data.save_to_sheets(
-    planday_query=planday_obj.get_shift_types,
-    google_client=google_client,
-    sheet_name='shift_types'
-)
-
-# Get all shifts
-Data.save_to_json(
-    planday_query=planday_obj.get_all_shifts,
-    data_dir=data_dir(),
-    title='all_shifts.json'
-)
-Data.save_to_sheets(
-    planday_query=planday_obj.get_all_shifts,
-    google_client=google_client,
-    sheet_name='all_shifts'
+    sheet_name='absence_requests'
 )
 
 # Get absence types
-Data.save_to_json(
+data_obj.save_to_json(
     planday_query=planday_obj.get_account_types,
     data_dir=data_dir(),
     title='account_types.json'
 )
-Data.save_to_sheets(
+data_obj.save_to_sheets(
     planday_query=planday_obj.get_account_types,
     google_client=google_client,
     sheet_name='account_types'
 )
 
-# Get absence requests
-Data.save_to_json(
-    planday_query=planday_obj.get_absence_requests,
+# Get all shifts
+data_obj.save_to_json(
+    planday_query=planday_obj.get_all_shifts,
     data_dir=data_dir(),
-    title='absence_requests.json'
+    title='all_shifts.json'
 )
-Data.save_to_sheets(
-    planday_query=planday_obj.get_absence_requests,
+data_obj.save_to_sheets(
+    planday_query=planday_obj.get_all_shifts,
     google_client=google_client,
-    sheet_name='absence_requests'
+    sheet_name='all_shifts'
+)
+
+departments_query = planday_obj.get_departments
+# Departments_list
+data_obj.save_to_json(
+    planday_query=departments_query,
+    data_dir=data_dir(),
+    title='departments_list.json'
+)
+data_obj.save_to_sheets(
+    planday_query=departments_query,
+    google_client=google_client,
+    sheet_name='departments_list'
+)
+
+# Employee groups
+data_obj.save_to_json(
+    planday_query=planday_obj.get_employee_groups,
+    data_dir=data_dir(),
+    title='employee_groups.json'
+)
+data_obj.save_to_sheets(
+    planday_query=planday_obj.get_employee_groups,
+    google_client=google_client,
+    sheet_name='employee_groups'
+)
+
+# Employees_list
+data_obj.save_to_json(
+    planday_query=planday_obj.get_employees,
+    data_dir=data_dir(),
+    title='employees_list.json'
+)
+data_obj.save_to_sheets(
+    planday_query=planday_obj.get_employees,
+    google_client=google_client,
+    sheet_name='employees_list'
+)
+
+# Get shift types
+data_obj.save_to_json(
+    planday_query=planday_obj.get_shift_types,
+    data_dir=data_dir(),
+    title='shift_types.json'
+)
+data_obj.save_to_sheets(
+    planday_query=planday_obj.get_shift_types,
+    google_client=google_client,
+    sheet_name='shift_types'
 )
