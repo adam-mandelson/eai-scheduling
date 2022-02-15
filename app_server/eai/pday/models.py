3e301b3287c8319ee1db3c912096e974899d84ed
adam.mandelson@gmail.com <adam.mandelson@gmail.com>
2022-02-15 14:04:49 -0500

    django init

---
+++ app_server/eai/pday/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Report(models.Model):
+    employeeName = models.CharField(max_length=50, blank=False, null=False)
+    month = models.CharField(max_length=50, blank=False, null=False)
+    dataTypes = models.CharField(max_lengh=50, blank=False, null=False)
+    value = models.IntegerField()
+    
