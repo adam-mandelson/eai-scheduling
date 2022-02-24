fe0c2a2c4d527579adbfc99911be149b5de71942
adam.mandelson@gmail.com <adam.mandelson@gmail.com>
2022-02-24 16:48:17 -0500

    Added update script pday.update_all

---
+++ pday/update_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from pday.update import PDayUpdate
+from pday.query import PDayQuery
+from pday.data import Data
+from pday.utils import auth_dir, data_dir
+
+PDayUpdate(
+    pday_obj=PDayQuery(
+        auth_dir=auth_dir()),
+    data_obj=Data(),
+    data_dir=data_dir()
+    ).update_all()
