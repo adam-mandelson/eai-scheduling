e1c48945287a94a733ffbfc085be34f2e4239250
adam.mandelson@gmail.com <adam.mandelson@gmail.com>
2022-02-24 17:03:06 -0500

    Added logging to update_all

--- pday/update_all.py
+++ pday/update_all.py
@@ -8,6 +8,8 @@ from pday.update import PDayUpdate
 from pday.query import PDayQuery
 from pday.data import Data
 from pday.utils import auth_dir, data_dir
+from time import gmtime, strftime
+
 
 PDayUpdate(
     pday_obj=PDayQuery(
@@ -15,3 +17,6 @@ PDayUpdate(
     data_obj=Data(),
     data_dir=data_dir()
     ).update_all()
+
+with open('last_update.txt', 'w') as f:
+    f.write(strftime('%d %b %Y %H:%M +0000', gmtime()))
